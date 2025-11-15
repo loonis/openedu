19cbd2977a9c6c6e6f1c4f5167a317d45252e034
lounis <loonis.net@protonmail.com>
2025-11-15 16:34:08 +0100

    fix: footer git build

---
+++ src/contexts/MenuContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface MenuContextType {
+  isMenuOpen: boolean;
+  openMenu: () => void;
+  closeMenu: () => void;
+}
+
+const MenuContext = createContext<MenuContextType | undefined>(undefined);
+
+export const MenuProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = () => setIsMenuOpen(true);
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <MenuContext.Provider value={{ isMenuOpen, openMenu, closeMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+export const useMenu = () => {
+  const context = useContext(MenuContext);
+  if (context === undefined) {
+    throw new Error('useMenu must be used within a MenuProvider');
+  }
+  return context;
+};
